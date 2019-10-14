5e5e3c3d5d1589257607c69ca2939ee1d4d80a38
nico <nico.vasquez@gmx.at>
2019-10-14 19:39:00 +0200

    Lesson 01-16: Set buttons id and get a reference of it

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -2,11 +2,13 @@ package at.htl.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.roll_button)
     }
 }
