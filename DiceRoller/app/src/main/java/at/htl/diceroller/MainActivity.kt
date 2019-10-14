eacd15a22055c13284e33063f2eadad3608bc192
nico <nico.vasquez@gmx.at>
2019-10-14 19:48:25 +0200

    Lesson 01-18: Add rollDice function that changes the text of the TextView to a random number

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package at.htl.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,19 @@ class MainActivity : AppCompatActivity() {
         var rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener {
-            Toast.makeText(this,"Pressed", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+
+
+    }
+
+    private fun rollDice() {
+
+        Toast.makeText(this,"Rolled dice", Toast.LENGTH_SHORT).show()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        var rand = Random.nextInt(6) + 1
+        resultText.text = rand.toString();
+
     }
 }
