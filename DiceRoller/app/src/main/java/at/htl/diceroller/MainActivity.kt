03c64e89654d26bc8ef730011cab347b168b0924
nico <nico.vasquez@gmx.at>
2019-10-14 20:00:23 +0200

    Lesson 01-21: Replaced TextView through ImageView for the DiceImages

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package at.htl.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener {
             rollDice()
@@ -25,9 +26,19 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,"Rolled dice", Toast.LENGTH_SHORT).show()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        var rand = Random.nextInt(6) + 1
-        resultText.text = rand.toString();
+        val rand = Random.nextInt(6) + 1
+
+        val drawableResource = when(rand){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
 
     }
 }
