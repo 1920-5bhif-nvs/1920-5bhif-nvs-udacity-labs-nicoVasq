fd4c23b133591c4d2ae77e03a30cb4d603005cf5
nico <nico.vasquez@gmx.at>
2020-02-19 14:07:51 +0100

    Lesson 05-22: Add ScoreViewModelFactory - Nico Vasquez

---
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+            // TODO Construct and return the ScoreViewModel
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
