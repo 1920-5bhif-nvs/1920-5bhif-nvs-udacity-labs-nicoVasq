661fa33d04820f8374056598cca755f07270a5f8
nico <nico.vasquez@gmx.at>
2020-02-19 12:22:32 +0100

    Lesson 05-08: Add GameViewModel - Nico Vasquez

---
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel(){
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+
+    override fun onCleared() {
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+}
