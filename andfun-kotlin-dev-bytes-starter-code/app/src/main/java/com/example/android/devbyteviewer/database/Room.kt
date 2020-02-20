c46364d22cb0ada2979c3e69da0b6e60add1b208
nico <nico.vasquez@gmx.at>
2020-02-20 16:36:07 +0100

    Lesson 09-10: Define Database and implement Singleton for it - Nico Vasquez

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
@@ -32,3 +33,20 @@ interface VideoDao {
     fun insertAll(vararg videos: DatabaseVideo)
 }
 
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+   synchronized(VideosDatabase::class.java) {
+       if(!::INSTANCE.isInitialized){
+           INSTANCE = Room.databaseBuilder(context.applicationContext,
+                   VideosDatabase::class.java,
+                   "videos").build()
+       }
+   }
+    return INSTANCE
+}
