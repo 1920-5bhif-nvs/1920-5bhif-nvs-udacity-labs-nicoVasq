17faef2999054a04b622f6aba028a567bce79adb
nico <nico.vasquez@gmx.at>
2020-02-20 13:49:21 +0100

    Lesson 08-08: Change type of response from plain string to List<MarsProperty> - Nico Vasquez

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -40,7 +40,7 @@ private val retrofit = Retrofit.Builder()
 interface MarsApiService {
     @GET("realestate")
     fun getProperties():
-            Call<String>
+            Call<List<MarsProperty>>
 }
 
 object MarsApi {
