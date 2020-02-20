ac42ef111232ae3fb660ec471c1614ef58ec22e6
nico <nico.vasquez@gmx.at>
2020-02-20 17:05:09 +0100

    Lesson 09-19: Add constraints for the work request - Nico Vasquez

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,9 +18,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -53,6 +52,17 @@ class DevByteApplication : Application() {
     }
 
     private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        setRequiresDeviceIdle(true)
+                    }
+                }.build()
+
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
                 1,
                 TimeUnit.DAYS)
