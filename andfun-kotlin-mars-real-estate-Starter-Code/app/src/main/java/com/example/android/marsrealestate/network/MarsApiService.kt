8b03215fa87831657b6afac7989687d3a9d236a5
nico <nico.vasquez@gmx.at>
2020-02-20 13:48:28 +0100

    Lesson 08-08: Add MoshiConverterFactory to retrofit - Nico Vasquez

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -21,18 +21,20 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
-private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .build()
 
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
+        .build()
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
         .build()
 
 interface MarsApiService {
