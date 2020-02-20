53d2b0e7f47940d20782ca41858f99ac2400aee4
nico <nico.vasquez@gmx.at>
2020-02-20 13:47:09 +0100

    Lesson 08-08: Create Moshi object using MoshiBuilder - Nico Vasquez

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,6 +17,8 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -27,6 +29,10 @@ private const val BASE_URL = "https://mars.udacity.com/"
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(ScalarsConverterFactory.create())
         .baseUrl(BASE_URL)
+        .build()
+
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
         .build()
 
 interface MarsApiService {
