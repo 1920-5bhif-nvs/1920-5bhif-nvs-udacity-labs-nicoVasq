d4a1cab976a39ef82242a4e9d8a7fe1c0200d059
nico <nico.vasquez@gmx.at>
2020-02-20 13:36:39 +0100

    Lesson 08-06: Create MarsApiInterface - Nico Vasquez

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,10 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
@@ -26,3 +28,9 @@ private val retrofit = Retrofit.Builder()
         .addConverterFactory(ScalarsConverterFactory.create())
         .baseUrl(BASE_URL)
         .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
